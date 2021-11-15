47c256494526860b6977b6f1107492ac6cbb26d4
tokyo-arch <83420072+tokyo-arch@users.noreply.github.com>
2021-11-16 01:16:27 +0530

    Rearrange an array with O(1) extra space
    Given an array arr[] of size N where every element is in the range from 0 to n-1. Rearrange the given array so that arr[i] becomes arr[arr[i]].
    
     
    
    Example 1:
    
    Input:
    N = 2
    arr[] = {1,0}
    Output: 0 1
    Explanation: 
    arr[arr[0]] = arr[1] = 0.
    arr[arr[1]] = arr[0] = 1.
     
    
    Example 2:
    
    Input:
    N = 5
    arr[] = {4,0,2,1,3}
    Output: 3 4 2 0 1
    Explanation: 
    arr[arr[0]] = arr[4] = 3.
    arr[arr[1]] = arr[0] = 4.
    and so on.

---
+++ Array/RearrangeArray.java
@@ -0,0 +1,39 @@
+class Rearrange 
+{
+    // The function to rearrange an array in-place so that arr[i]
+    // becomes arr[arr[i]].
+    void rearrange(int arr[], int n) 
+    {
+        // First step: Increase all values by (arr[arr[i]]%n)*n
+        for (int i = 0; i < n; i++)
+            arr[i] += (arr[arr[i]] % n) * n;
+
+        // Second Step: Divide all values by n
+        for (int i = 0; i < n; i++)
+            arr[i] /= n;
+    }
+
+    // A utility function to print an array of size n
+    void printArr(int arr[], int n) 
+    {
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println("");
+    }
+
+    /* Driver program to test above functions */
+    public static void main(String[] args) 
+    {
+        Rearrange rearrange = new Rearrange();
+        int arr[] = {3, 2, 0, 1};
+        int n = arr.length;
+        
+        System.out.println("Given Array is :");
+        rearrange.printArr(arr, n);
+
+        rearrange.rearrange(arr, n);
+
+        System.out.println("Modified Array is :");
+        rearrange.printArr(arr, n);
+    }
+}
